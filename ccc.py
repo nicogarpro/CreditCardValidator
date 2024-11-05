bdcff32c492d18e5408855e617442b0e4d94f1a0
NICOGARPRO <69008744+nicogarpro@users.noreply.github.com>
2024-11-05 19:15:48 -0300

    Update ccc.py
    Added Visa and American Express (Amex) as IINs

--- ccc.py
+++ ccc.py
@@ -18,18 +18,46 @@ seventhdigit = card_str[6]
 eighthdigit = card_str[7]
 ninthdigit = card_str[8]
 tenthdigit = card_str[9]
-
-
+eleventhdigit = card_str[10]
+twelvethdogot = card_str[11]
+thirteenthdigit = card_str[12]
+fourteenthdigit = card_str[13]
+fifteenthdigit = card_str[14]
+sixteenthdigit = card_str[15]
 
 # Check Industry Protocol
 
 if firstdigit == "5" and seconddigit == "1":
     print("Mastercard")
     mastercard = True
+    visa = False
+    americanexpress = False
 
 elif firstdigit == "5" and seconddigit == "5":
     print('Mastercard')
     mastercard = True
+    visa = False
+    americanexpress = False
+
+elif firstdigit == "4":
+    print("Visa")
+    visa = True
+    mastercard = False
+    americanexpress = False
+
+elif firstdigit == "3" and seconddigit == "4":
+    print("American Express")
+    americanexpress = True
+    visa = False
+    mastercard = False
+
+elif firstdigit == "3" and seconddigit == "7":
+    print("American Express")
+    americanexpress = True
+    visa = False
+    mastercard = False
 
 else:
-    mastercard = False
+    print("Not valid with current database")
+
+
